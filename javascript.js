a61690c8f65125b038df555ebab8e247d240188c
AkioKiyota <berkefekeskin@gmail.com>
2022-07-09 20:11:52 +0300

    x

--- javascript.js
+++ javascript.js
@@ -1,1 +1,21 @@
+window.onscroll = function()
+{
+    scrollFunc();
+}
 
+var scrollValue = 15;
+var scrollVarA = document.body.scrollTop;
+var scrollVarB = document.documentElement.scrollTop;
+
+
+
+function scrollFunc()
+{
+    if(scrollVarA > scrollValue || scrollVarB > scrollValue)
+    {
+        
+    }else
+    {
+        
+    }
+}
