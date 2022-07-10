19a845a52dca5c2f8de72d4c993d5746184c0a3a
AkioKiyota <berkefekeskin@gmail.com>
2022-07-10 16:00:01 +0300

    asd

---
+++ Games/javascript.js
@@ -0,0 +1,34 @@
+window.onscroll = function()
+{
+    scrollFunc();
+}
+
+var scrollValue = 200;
+
+var topContainer = document.getElementById('top-container').style;
+var logo = document.getElementById('logo').style
+
+function scrollFunc()
+{
+    if(document.body.scrollTop > scrollValue || document.documentElement.scrollTop > scrollValue)
+    {
+        topContainer.height = '40px';
+        Logo();
+    }else
+    {
+        topContainer.height = '60px';
+        LogoR();
+    }
+}
+
+function Logo()
+{
+    logo.fontSize = '20px';
+    logo.padding = '6px 20px';
+}
+
+function LogoR()
+{
+    logo.fontSize = '35px';
+    logo.padding = '6px 20px';
+}
