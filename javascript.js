a7eb3f06da4424cab4a91601106a00d0fad76ea8
AkioKiyota <berkefekeskin@gmail.com>
2022-07-10 13:48:07 +0300

    d

--- javascript.js
+++ javascript.js
@@ -3,19 +3,32 @@ window.onscroll = function()
     scrollFunc();
 }
 
-var scrollValue = 15;
-var scrollVarA = document.body.scrollTop;
-var scrollVarB = document.documentElement.scrollTop;
+var scrollValue = 200;
 
-
+var topContainer = document.getElementById('top-container').style;
+var logo = document.getElementById('logo').style
 
 function scrollFunc()
 {
-    if(scrollVarA > scrollValue || scrollVarB > scrollValue)
+    if(document.body.scrollTop > scrollValue || document.documentElement.scrollTop > scrollValue)
     {
-        
+        topContainer.height = '40px';
+        Logo();
     }else
     {
-        
+        topContainer.height = '60px';
+        LogoR();
     }
+}
+
+function Logo()
+{
+    logo.fontSize = '20px';
+    logo.padding = '11px 5px';
+}
+
+function LogoR()
+{
+    logo.fontSize = '35px';
+    logo.padding = '11px 20px';
 }
